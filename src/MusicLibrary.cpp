7a2b43cf09ba38de461f2793002de898ab018796
Andreas Mika <anmimt01@hs-esslingen.de>
2023-12-12 16:13:49 +0100

    Hinzufügen von Funktionsaufrufen

--- src/MusicLibrary.cpp
+++ src/MusicLibrary.cpp
@@ -1,1 +1,42 @@
 #include "MusicLibrary.hpp"
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+
+void MusicLibrary::addSong()
+{
+    std::cout << "Songs hinzufügen ";
+}
+
+void MusicLibrary::editSong()
+{
+    std::cout << "Songs bearbeiten ";
+}
+
+void MusicLibrary::deleteSong()
+{
+    std::cout << "Songs löschen ";
+}
+
+void MusicLibrary::searchLibrary()
+{
+    std::cout << "Bibliothek durchsuchen ";
+}
+
+void MusicLibrary::displayLibrary()
+{
+    std::cout << "Bibliothek anzeigen ";
+}
+
+void MusicLibrary::initializeLibrary()
+{
+    std::cout << "Bibliothek initialisieren ";
+}
+
+void MusicLibrary::saveLibrary()
+{
+    std::cout << "Bibliothek speichern ";
+}
